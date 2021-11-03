297493a86ffcfecfdb3ffde4bbfac80b506b4033
Pratik <pratikraval2603@gmail.com>
2021-11-03 16:05:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/NavBar';
+import Register from './Components/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
