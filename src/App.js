1c47e420ed1c62623bd1fcb7ae60c9d903939114
Pratik <pratikraval2603@gmail.com>
2021-11-09 19:27:09 +0530

    delete,authentication&update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 // import logo from './logo.svg';
-import './App.css';
-import Display from './Components/Display';
-import NavBar from './Components/NavBar';
-import Register from './Components/Register';
+import "./App.css";
+import Display from "./Components/Display";
+// import NavBar from "./Components/NavBar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Register from "./Components/Register";
+import NavBar from "./Components/NavBar";
+import Login from "./Components/Login";
+import Logout from "./Components/Logout";
+import Update from "./Components/Update";
+// import { Fragment } from "react";
+// import Register from './Components/Register';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    
+      <Router>
       <NavBar/>
-      <Display/>
-      {/* <Register/> */}
+    <div className="App">
+      <>
+      {/* <Switch>
+          <Route exact path="/login">
+            <Login/>
+          </Route>
+          <Route exact path="/register">
+            <Register/>
+          </Route>
+          <Route exact path="/">
+            <Display/>
+          </Route>
+          <Route exact path="/logout">
+            <Logout/>
+          </Route>
+      </Switch> */}
+        {/* <Homepage/> */}
+        {/* <Login/> */}
+        {/* <Register/> */}
+        <Update/>
+
+        
+      </>
     </div>
+    </Router>
+    </>
   );
 }
 
